23b6503b6e5a8fd451a1ae40f0f00128ed48826b
John <john.bernal11@misena.edu.co>
2023-06-07 11:53:15 -0500

    Initial commit

---
+++ src/main/java/com/example/configserveraccount/ConfigServerAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.configserveraccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/configserveraccount/ConfigServerAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserveraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
